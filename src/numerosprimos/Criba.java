3f7c3345e51f519984f76d7e557c9cc05a04c2cd
josecayuela5 <(none)>
2024-02-28 21:34:08 +0100

    Se agregan los comentarios para construir el documento de JavaDoc de las clases Criba y Main.

--- src/numerosprimos/Criba.java
+++ src/numerosprimos/Criba.java
@@ -1,7 +1,27 @@
 package numerosprimos;
 
+/**
+ * Genera el conjunto de números primos desde 2 hasta un valor máximo especificado por el usuario.
+ * @author Jose
+ * @see <a href = "https://es.wikipedia.org/wiki/N%C3%BAmero_primo"> Numeros primos </a>
+ */
+
 public class Criba {
+
+    /**
+     * Array de booleanos en la que se van almacenando valores hasta max para determinar los números primos y no primos.
+     */
     private static boolean[] esPrimo;
+
+    /**
+     * Si max se establece en 2 o superior genera un vector con los números primos hasta max. Si es menor devuelve un vector vacío.
+     * @param max
+     * Valor máximo hasta el cual se determinarán los números primos.
+     * @return <ul>
+     *     <li>Vector con los números primos hasta el valor máximo a partir del número 2.</li>
+     *     <li>Vector vacío si el valor máximo se establece en 1 o inferior.</li>
+     * </ul>
+     */
 
     public static int[] generarPrimos(int max) {
         if (max >= 2) {
@@ -13,13 +33,18 @@ public class Criba {
 
             return vectorPrimos;
         } else {
-            int[] emptyArray=new int[0];
+            int[] emptyArray = new int[0];
 
             return emptyArray;
         }
     }
 
-    private static int[] rellenarPrimos() {
+    /**
+     * Crea un array de la longitud adecuada, para posteriormente agregar paulatinamente los números primos.
+     * @return
+     * Vector con los números primos hasta max.
+     */
+    public static int[] rellenarPrimos() {
         int totalPrimos = contarPrimos();
         int[] primos = new int[totalPrimos];
         int insertionIndex = 0;
@@ -33,7 +58,13 @@ public class Criba {
         return primos;
     }
 
-    private static int contarPrimos() {
+    /**
+     * Cuenta cuantos valores "true" existen en el array "esPrimo".
+     * @return
+     * Valor entero con el número total de primos.
+     */
+
+    public static int contarPrimos() {
         int totalPrimos = 0;
 
         for (boolean b : esPrimo) {
@@ -45,7 +76,12 @@ public class Criba {
         return totalPrimos;
     }
 
-    private static void cribarPrimos() {
+    /**
+     * Se van estableciendo valores "false" en la tabla "esPrimo" para aquellos números no primos, hasta llegar a un valor que sea superior a la raíz cuadrada del número máximo.
+     * @see <a href = "https://es.wikipedia.org/wiki/Criba_de_Erat%C3%B3stenes">Criba de Erastótenes</a>
+     */
+
+    public static void cribarPrimos() {
 
         for (int i = 0; i < Math.sqrt(esPrimo.length); i++) {
             if (esPrimo[i]) {
@@ -57,7 +93,11 @@ public class Criba {
         }
     }
 
-    private static void inicializarArray() {
+    /**
+     * Inicializa los valores del array "esPrimo" con el valor booleano "true" a partir del número 2.
+     */
+
+    public static void inicializarArray() {
 
         for (int i = 2; i < esPrimo.length; i++) {
             esPrimo[i] = true;
--- src/numerosprimos/Main.java
+++ src/numerosprimos/Main.java
@@ -2,6 +2,11 @@ package numerosprimos;
 
 import java.util.Scanner;
 
+/**
+ * Clase principal, ejecuta el programa.
+ * @author Jose
+ */
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -26,7 +31,11 @@ public class Main {
             System.out.print(numeroPrimo + "\t");
         }
     }
-    private static void separarEnFilas(int i) {
+
+    /**
+     * Cada 10 valores mostrados por consola inserta un salto de línea
+     */
+    public static void separarEnFilas(int i) {
         if (i % 10 == 0) System.out.println("\b");
     }
 }
